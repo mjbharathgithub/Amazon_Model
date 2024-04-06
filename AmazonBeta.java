411e5f7e85c20b69b1d335a6f5effa39b92be0c3
Alagar03 <alagarrajendran3@gmail.com>
2024-04-06 08:50:29 +0530

    Sample

---
+++ AmazonBeta.java
@@ -0,0 +1,17 @@
+package AmazonBeta;
+
+import java.util.Scanner;
+
+public class AmazonBeta {
+    Admin adminObj = new Admin
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER YOUR IDENTITY: \n1.ADMIN \n2.SELLER \n3.CUSTOMER ");
+        int choosen = sc.nextInt();
+        if(choosen == 1){
+            System.out.println("ENTER ADMIN PASSWORD: ");
+            String pass = sc.nextLine();
+            if()
+        }
+    }
+}
