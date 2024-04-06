8ec54f3e105c4b69892c1cf17127af7ced1c0a6e
mjbharathgithub <mjbharath2003@gmail.com>
2024-04-06 10:36:41 +0530

    UPDATE CUSTOMER.JAVA

--- Customer.java
+++ Customer.java
@@ -1,4 +1,7 @@
+package amazonBeta.AmazonBeta;
+
 import java.util.*;
+
 class Customer{
     String CustomerName = "";
 public Customer(String name ){
@@ -7,10 +10,29 @@ public Customer(String name ){
     List<Product> cart = new ArrayList<>();
     
     public void addToCart(Product product) {
-    	cart.add(product);//add each product to cart
+    	this.cart.add(product);//add each product to cart
+    	System.out.println(product+" IS ADDED TO YOUR CART");
+    	
 		
 	}
     
+    public void removeFromCart(Product product) {
+    	this.cart.remove(product);
+    	System.out.println(product+" IS REMOVED FROM YOUR CART");
+		
+	}
+    
+    public void viewCart() {
+        System.out.println("YOUR CART");
+        this.cart.forEach(product -> System.out.println("PRODUCT NAME : " + product.product_name +
+                                                         "\nPRICE : " + product.price +
+                                                         "\nQUANTITY : " + product.quantity));
+    }
+
+    public void clearCart() {
+    	this.cart.clear();
+		
+	}
     public List<Product> placeOrder() {
     	return this.cart;//return all the product added when orer placed
 		}
